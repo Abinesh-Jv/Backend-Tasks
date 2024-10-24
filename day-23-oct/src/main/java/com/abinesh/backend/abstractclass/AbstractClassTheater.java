14b27a273da1ccefd99fb9a010156586a7d9bd38
Abinesh-Jv <103888564+Abinesh-Jv@users.noreply.github.com>
2024-10-24 14:11:02 +0530

    Add files via upload

---
+++ day-23-oct/src/main/java/com/abinesh/backend/abstractclass/AbstractClassTheater.java
@@ -0,0 +1,10 @@
+package com.abinesh.backend.abstractclass;
+
+public class AbstractClassTheater extends AbstractClass {
+    protected String getDirection(){
+        return "the direction for this theater is to take the highway for 10km";
+    }
+
+
+
+}
